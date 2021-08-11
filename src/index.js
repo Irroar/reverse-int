541387daf97809cfd98132ed6ab74c584e15765d
Iryna Lytvynenko <frau4145@gmail.com>
2021-08-11 22:56:36 +0300

    Add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function reverse (n) {
-  
+    let numberStr = ''
+    if (n < 0) { numberStr = n.toString().split('').slice(1); }
+    else { numberStr = n.toString().split(''); }
+    
+    let reversedString = [];
+    for (let i = 0; i < numberStr.length; i++) {
+        reversedString.unshift(numberStr[i])
+    }
+    return Number.parseInt(reversedString.join(''))
 }
+
